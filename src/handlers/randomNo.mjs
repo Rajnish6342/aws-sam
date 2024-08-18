303aa17255c4823309c648647633d33cc9894414
Rajnish <rajnish@tataaig.com>
2024-08-18 14:31:55 +0530

    added mathrandom

--- src/handlers/randomNo.mjs
+++ src/handlers/randomNo.mjs
@@ -6,7 +6,8 @@ export const randomNo = async (event, context) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        result: new Date().getTime()
+        result: new Date().getTime(),
+        randomNo: Math.random()
       })
     };
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
