ae4d44fae502e2e4d788e63b7f65cf47f9adf5a5
Rajnish <rajnish@tataaig.com>
2024-08-18 13:37:11 +0530

    added code

---
+++ src/handers/randomNo.mjs
@@ -0,0 +1,15 @@
+
+export const randomNo = async (event, context) => {
+    console.info('context:', context);
+    console.info('received:', event);
+   
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        result: new Date().getTime()
+      })
+    };
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+  }
+  
